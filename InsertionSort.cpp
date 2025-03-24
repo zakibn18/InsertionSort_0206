978f5f4b5a33f4556fd14c23afa7b2db087b1445
zakibn18 <jekben815@gmail.com>
2025-03-24 22:40:53 +0700

    create void display

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -44,7 +44,19 @@ void insertionSort(){
   }
 }
 
+void display(){
+  cout << endl;
+  cout << "=================================" << endl;
+  cout << "Total Pass = " << n - 1 << endl;
+  cout << "=================================" << endl;
+  cout << "Element Array yang telah tersusun" << endl;
+  cout  << "================================" << endl;
 
+  for (int j = 0; j > n; j++){
+    cout << arr[j] << endl;
+  }
+  cout << endl;
+}
 
 int main(){
 
