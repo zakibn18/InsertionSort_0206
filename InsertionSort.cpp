04bc51c69d49f94fb02c8838f7d40c143b27dd3d
zakibn18 <jekben815@gmail.com>
2025-03-24 22:28:08 +0700

    create void input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,6 +4,30 @@ using namespace std;
 int arr[20];
 int n;
 
+void input(){
+  while(true){
+    cout << "Masukkan Jumlah Data Pada Array : ";
+    cin >> n;
+    
+    if (n <= 20){
+      break;
+    } else{
+      cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
+    }
+  }
+  cout << endl;
+  cout << "======================" << endl;
+  cout << "Masukkan Element Array" << endl;
+  cout << "======================" << endl;
+
+  for (int i = 0; i < n; i++){
+    cout << "Data ke -" << (i + 1) << ": ";
+    cin >> arr[i];
+  }
+}
+
+
+
 int main(){
 
 }
