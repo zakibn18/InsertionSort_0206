96914ccf2f0c1107627e8f5bc0ac39e9ac8c230f
zakibn18 <jekben815@gmail.com>
2025-03-24 22:34:58 +0700

    create void insertSort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -26,6 +26,24 @@ void input(){
   }
 }
 
+void insertionSort(){
+  int temp;
+  int j, i;
+
+  for (i = 1; i <= n - 1; i++){
+    temp = arr[i];
+
+    j = i - 1;
+
+    while (j >= 0 && arr[j] > temp){
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = temp;
+  }
+}
+
 
 
 int main(){
